cc56a259a2a0629def03ad76e3f4b3ec2d37fbc6
abdul rahimi <afr114@gmail.com>
2015-12-19 19:10:25 -0800

    add initial files

---
+++ app/src/main/java/com/example/guest/recommendations/api/Etsy.java
@@ -0,0 +1,8 @@
+package com.example.guest.recommendations.api;
+
+/**
+ * Created by Guest on 12/19/15.
+ */
+public class Etsy {
+    private static final String API_KEY = "bubkg6948qk0zfevdpwxzu3h";
+}
